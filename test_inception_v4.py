70ab01e42f8048be49efced33856fd90d82de0f7
lajos <panka.nospam@gmail.com>
2018-04-02 12:53:47 -0400

    wip

---
+++ test_inception_v4.py
@@ -0,0 +1,67 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import os
+import sys
+
+import_paths = ['../models/research/slim']
+for import_path in import_paths:
+    if import_path not in sys.path:
+        sys.path.append(import_path)
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+try:
+    import urllib2 as urllib
+except ImportError:
+    import urllib.request as urllib
+
+from datasets import imagenet
+from nets import inception
+from preprocessing import inception_preprocessing
+
+from tensorflow.contrib import slim
+
+checkpoints_dir = 'models/inception_v4'
+
+image_size = inception.inception_v4.default_image_size
+
+#image_size = inception.inception_v1.default_image_size
+
+with tf.Graph().as_default():
+    url = 'https://upload.wikimedia.org/wikipedia/commons/7/70/EnglishCockerSpaniel_simon.jpg'
+    image_string = urllib.urlopen(url).read()
+    image = tf.image.decode_jpeg(image_string, channels=3)
+    processed_image = inception_preprocessing.preprocess_image(image, image_size, image_size, is_training=False)
+    processed_images  = tf.expand_dims(processed_image, 0)
+    
+    # Create the model, use the default arg scope to configure the batch norm parameters.
+    with slim.arg_scope(inception.inception_v1_arg_scope()):
+        logits, _ = inception.inception_v4(processed_images, num_classes=1001, is_training=False)
+    probabilities = tf.nn.softmax(logits)
+    
+    init_fn = slim.assign_from_checkpoint_fn(
+        os.path.join(checkpoints_dir, 'inception_v4.ckpt'),
+        slim.get_model_variables('InceptionV4'))
+    
+    with tf.Session() as sess:
+        init_fn(sess)
+        np_image, probabilities = sess.run([image, probabilities])
+        probabilities = probabilities[0, 0:]
+        sorted_inds = [i[0] for i in sorted(enumerate(-probabilities), key=lambda x:x[1])]
+        
+    plt.figure()
+    plt.imshow(np_image.astype(np.uint8))
+    plt.axis('off')
+    plt.show()
+
+    names = imagenet.create_readable_names_for_imagenet_labels()
+    for i in range(5):
+        index = sorted_inds[i]
+        print('Probability %0.2f%% => [%s]' % (probabilities[index] * 100, names[index]))
